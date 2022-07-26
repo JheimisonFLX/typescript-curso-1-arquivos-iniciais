93b0dfe40350810b56bdaf68b6b22379af678825
JheimisonFLX <jheimison_007@hotmail.com>
2022-07-26 09:16:04 -0400

    03 - Aula 12

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -15,13 +15,21 @@ export class NegociacaoController {
     adiciona(): void {
         const negociacao = this.criaNegociacao();
         console.log(negociacao);
+        this.limpaFormulario();
     }
 
-    criaNegociacao(): Negociacao{
+    criaNegociacao(): Negociacao {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
         return new Negociacao(date, quantidade, valor);
+    }
+
+    limpaFormulario(): void {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
     }
 }
