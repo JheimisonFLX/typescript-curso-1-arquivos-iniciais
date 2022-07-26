380774ef379cc3342dc4da14f0f2018599ef38ac
JheimisonFLX <jheimison_007@hotmail.com>
2022-07-26 16:01:09 -0400

    05 - Aula 08

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,13 +1,13 @@
 import { Negociacao } from "./negociacao.js";
 
-export class Negociacoes{
-    private negociacoes: Array<Negociacao> = [];
+export class Negociacoes {
+    private negociacoes: Negociacao[] = [];
 
-    adciona(negociacao: Negociacao){
+    adciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
 
-    lista(): ReadonlyArray<Negociacao>{
+    lista(): readonly Negociacao[] {
         return this.negociacoes;
     }
 }
